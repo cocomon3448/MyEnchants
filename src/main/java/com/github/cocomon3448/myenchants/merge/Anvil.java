3ab2493b555001e89fe759727f3bae5da9f689ca
cocomon3448 <73870100+cocomon3448@users.noreply.github.com>
2022-02-06 21:43:48 +0900

    3nd Update(1/2 Anvil Event)

--- src/main/java/com/github/cocomon3448/myenchants/merge/Anvil.java
+++ src/main/java/com/github/cocomon3448/myenchants/merge/Anvil.java
@@ -2,6 +2,7 @@ package com.github.cocomon3448.myenchants.merge;
 
 
 import com.github.cocomon3448.myenchants.tools.MergedEnchants;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
@@ -33,18 +34,51 @@ public class Anvil implements Listener {
         ArrayList<String> lores = new ArrayList<>();
 
         if(fItem.getItemMeta().hasEnchant(homingEnchantments) && sItem.getItemMeta().hasEnchant(homingEnchantments)) {
-            int fHNo = fItem.getItemMeta().getEnchantLevel(homingEnchantments);
-            int sHNo = sItem.getItemMeta().getEnchantLevel(homingEnchantments);
-            int rHomNo = MergedEnchants.getLevel(fHNo,sHNo);
+            if(isBow(fItem) == true && isBow(sItem) == true) {
+                System.out.println("BOW&BOW");
+                int fHomNo = fItem.getItemMeta().getEnchantLevel(homingEnchantments);
+                int sHomNo = sItem.getItemMeta().getEnchantLevel(homingEnchantments);
+                int rHomNo = MergedEnchants.getLevel(fHomNo, sHomNo, homingEnchantments);
 
-            ItemMeta mata = result.getItemMeta();
-            lores.add(homingEnchantments.getFormattedName(rHomNo));
-            mata.setLore(lores);
-            result.setItemMeta(mata);
-            result.addUnsafeEnchantment(homingEnchantments, rHomNo);
-            System.out.println(fHNo+" "+sHNo);
-            e.setResult(result);
+                ItemMeta mata = result.getItemMeta();
+                lores.add(homingEnchantments.getFormattedName(rHomNo));
+                mata.setLore(lores);
+                result.setItemMeta(mata);
+                result.addUnsafeEnchantment(homingEnchantments, rHomNo);
+                System.out.println(fHomNo+" "+sHomNo);
+                e.setResult(result);
+            }
+            if(isBow(fItem) == true && isBook(sItem) == true) {
+                System.out.println("BOW&BOOK");
+                int fHomNo = fItem.getItemMeta().getEnchantLevel(homingEnchantments);
+                int sHomNo = sItem.getItemMeta().getEnchantLevel(homingEnchantments);
+                int rHomNo = MergedEnchants.getLevel(fHomNo, sHomNo, homingEnchantments);
+
+                ItemMeta mata = result.getItemMeta();
+                lores.add(homingEnchantments.getFormattedName(rHomNo));
+                mata.setLore(lores);
+                result.setItemMeta(mata);
+                result.addUnsafeEnchantment(homingEnchantments, rHomNo);
+                System.out.println(fHomNo+" "+sHomNo);
+                e.setResult(result);
+            }
         }
     }
 
+    public static boolean isBook(ItemStack item) {
+        if(item.getType().equals(Material.ENCHANTED_BOOK)) {
+            return true;
+        } else {
+            System.out.println("Is Book = false "+ item.getType());
+            return false;
+        }
+    }
+    public static boolean isBow(ItemStack item) {
+        if(item.getType().equals(Material.BOW)) {
+            return true;
+        } else {
+            System.out.println("Is Bow = false "+ item.getType());
+            return false;
+        }
+    }
 }
--- src/main/java/com/github/cocomon3448/myenchants/tools/MergedEnchants.java
+++ src/main/java/com/github/cocomon3448/myenchants/tools/MergedEnchants.java
@@ -1,7 +1,10 @@
 package com.github.cocomon3448.myenchants.tools;
 
+import com.github.cocomon3448.myenchants.MyEnchants;
+import org.bukkit.enchantments.Enchantment;
+
 public class MergedEnchants {
-    public static int getLevel(int first, int second) {
+    public static int getLevel(int first, int second, Enchantment enc) {
         if(first>second) {
             return first;
         }
@@ -9,7 +12,11 @@ public class MergedEnchants {
             return second;
         }
         if(first == second) {
-            return first + 1;
+            if((first+1) > enc.getMaxLevel()) {
+                return enc.getMaxLevel();
+            } else{
+                return first + 1;
+            }
         }
         return -1;
     }
